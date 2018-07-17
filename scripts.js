5270769c01fa3b6dc7e6cd5296cbdcc51954a1ae
Steve Smith <stevesmithsays@gmail.com>
2018-07-16 22:48:16 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let statement = "Feel free to reach me at (847)845-0219 or by email: ";
+
+let email = 'stevesmithsays@gmail.com';
+let greeting= statement + email;
+
+
+const alertFunc = () => {
+  alert(greeting);
+  
+};
